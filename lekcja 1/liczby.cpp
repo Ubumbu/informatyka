329d058d08aa111f38043eb79eb933347d629644
Oskar Impert (Uczeń) <oimpert688@technikumkreatywne.pl>
2024-10-31 10:14:24 +0100

    Komentarz

---
+++ lekcja 1/liczby.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int A = 5;
+	int B = 3;
+	int suma = A+B;
+	int roznica = A-B;
+	int iloczyn = A*B;
+	cout << "A" << A << endl;
+	cout << "B"	<< B << endl;
+	cout << "suma " << suma << endl;
+	cout << "roznica " << roznica << endl;
+	cout <<  "iloczyn" << iloczyn << endl;
+	return 0
+}
